bcbcbb3f453a4d735e214a6419f0aed5f24d3c2d
Silas Owu <owusilasyp00@gmail.com>
2024-08-09 03:58:18 -0700

    🔧 Chore: add design system to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,27 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    fontFamily: {
+      'outfit':["Outfit", "sans-serif"]
+    },
+    colors:{
+      'mirage':'#1A2A33',
+      'tepapagreen':'#1F3641',
+      'casper':'#A8BFC9',
+      'casper':'#A8BFC9',
+      'botticelli':'#DBE8ED',
+      'goldentainoi':'#FFC860',
+      'saffron':'#F2B137',
+      'turquoiseblue':'#65E9E4',
+      'turquoiseblue':'#65E9E4',
+      'turquoise':'#31C3BD',
+      'btnoffset_color':'#CC8B13',
+    },
+    screens:{
+      'mobile': {'min':'200px','max':'427px'},
+      'tablet': {'min':'427px','max':'782px'},
+      'laptop': {'min':'782.1px'}
+    },
     extend: {},
   },
   plugins: [],
