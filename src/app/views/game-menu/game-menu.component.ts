afdeeb02edbba230ca64c5928be256191a53eef7
Silas Owu <owusilasyp00@gmail.com>
2024-08-09 08:19:44 -0700

    💄 Style: create ui for game-menu view

--- src/app/views/game-menu/game-menu.component.ts
+++ src/app/views/game-menu/game-menu.component.ts
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
   templateUrl: './game-menu.component.html',
   styleUrl: './game-menu.component.css'
 })
-export class GameMenuComponent {
 
+export class GameMenuComponent {
+  markXSelected: boolean = false;
+  markOSelected: boolean = true;
+
+  selectMark(mark: string){
+    if(mark === 'x'){
+      this.markXSelected = !this.markXSelected;
+      this.markOSelected = false;
+    }else if(mark === 'o'){
+      this.markOSelected =!this.markOSelected;
+      this.markXSelected = false;
+    }
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,27 +5,28 @@ module.exports = {
   ],
   theme: {
     fontFamily: {
-      'outfit':["Outfit", "sans-serif"]
+      'outfit':["Outfit", "sans-serif"],
+      'varelaround':["Varela Round", "sans-serif"]
     },
     colors:{
       'mirage':'#1A2A33',
       'tepapagreen':'#1F3641',
       'casper':'#A8BFC9',
-      'casper':'#A8BFC9',
       'botticelli':'#DBE8ED',
       'goldentainoi':'#FFC860',
       'saffron':'#F2B137',
-      'turquoiseblue':'#65E9E4',
-      'turquoiseblue':'#65E9E4',
       'turquoise':'#31C3BD',
-      'btnoffset_color':'#CC8B13',
     },
     screens:{
       'mobile': {'min':'200px','max':'427px'},
       'tablet': {'min':'427px','max':'782px'},
       'laptop': {'min':'782.1px'}
     },
-    extend: {},
+    extend: {
+      boxShadow: {
+        'inner-bottom': 'inset 0 -7.5px 1px rgba(0, 0, 0, 0.3)',
+      },
+    },
   },
   plugins: [],
 }
